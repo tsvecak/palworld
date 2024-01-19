a2bbfcb9da49bd7076f67417dbd3b85cac60dab3
DESKTOP-OI2P5CB\Aless <alessandro0223@gmail.com>
2024-01-19 21:29:16 +0100

    Back button update

--- src/components/buttons/BackButton.tsx
+++ src/components/buttons/BackButton.tsx
@@ -1,14 +1,15 @@
 'use client';
 import { useRouter } from 'next/navigation';
 
-const BackButton = () => {
+const BackButton = ({ label = 'Back' }) => {
   const router = useRouter();
   return (
     <a
       onClick={() => router.back()}
       className="mb-auto cursor-pointer underline underline-offset-2"
     >
-      {`< `}Back to Paldeck
+      {`< `}
+      {label}
     </a>
   );
 };
