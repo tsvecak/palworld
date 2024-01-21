de8a2b54eeb3a3c6833fcf730066abdf7dfc9f19
tsvecak <tsvecak@gmail.com>
2024-01-21 03:31:44 +0000

    Removign slice

--- src/components/PalsSpotlight.tsx
+++ src/components/PalsSpotlight.tsx
@@ -10,7 +10,7 @@ const PalsSpotlight = async () => {
     <div className="w-full">
       <h3 className="text-dark mb-4 dark:text-white">Pals Spotlight</h3>
       <div className="grid w-full max-w-full grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-4">
-        {data.slice(0, 3).map((pal: Pal) => (
+        {data.map((pal: Pal) => (
           <PalCard key={pal.id} pal={pal} />
         ))}
       </div>
